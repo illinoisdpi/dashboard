c2fc0064d557d5a4bb2ee278a1510658f37f410c
Raghu Betina <raghubetina@users.noreply.github.com>
2023-03-14 16:19:10 -0500

    Wrap in transaction

--- app/models/piazza_activity_download.rb
+++ app/models/piazza_activity_download.rb
@@ -34,30 +34,32 @@ class PiazzaActivityDownload < ApplicationRecord
   after_create :process_csv
 
   def process_csv
-    self.csv_filename = csv_file.original_filename
+    ActiveRecord::Base.transaction do
+      self.csv_filename = csv_file.original_filename
 
-    options = {key_mapping: {"live_q&a_upvotes": :live_qa_upvotes}}
+      options = {key_mapping: {"live_q&a_upvotes": :live_qa_upvotes}}
 
-    csv = SmarterCSV.process(csv_file, options)
+      csv = SmarterCSV.process(csv_file, options)
 
-    csv.each do |row|
-      emails = row.fetch(:emails).split(";").map(&:strip)
+      csv.each do |row|
+        emails = row.fetch(:emails).split(";").map(&:strip)
 
-      user = User.where(email: emails).first
-      
-      if user.blank?
-        user = User.new(email: emails.first, password: SecureRandom.hex(16))       
+        user = User.where(email: emails).first
+        
+        if user.blank?
+          user = User.new(email: emails.first, password: SecureRandom.hex(16))       
+        end
+
+        user.piazza_full = row.fetch(:name, "None provided")
+        user.save
+
+        enrollment = Enrollment.find_or_create_by(user: user, cohort: cohort) do |the_enrollment|
+          the_enrollment.role = row.fetch(:role)
+        end
+
+        piazza_activity_breakdown = piazza_activity_breakdowns.
+          create(enrollment:, **row)
       end
-
-      user.piazza_full = row.fetch(:name, "None provided")
-      user.save
-
-      enrollment = Enrollment.find_or_create_by(user: user, cohort: cohort) do |the_enrollment|
-        the_enrollment.role = row.fetch(:role)
-      end
-
-      piazza_activity_breakdown = piazza_activity_breakdowns.
-        create(enrollment:, **row)
     end
   end
 end
