ea1ff873039c8de2e862a1978ba95efccaddff50
Raghu Betina <raghubetina@users.noreply.github.com>
2023-03-14 16:13:10 -0500

    Assume all requests are from Central timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  around_action :set_time_zone
+
+  private
+
+  def set_time_zone(&block)
+    Time.use_zone("America/Chicago", &block)
+  end
 end
