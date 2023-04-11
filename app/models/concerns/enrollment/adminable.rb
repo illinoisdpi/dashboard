5e5ba2a5f9814ed961cb907021ebc6503960caf1
Ian Heraty <heratyian@gmail.com>
2023-04-11 10:00:38 -0500

    Fix issue with searching by user name from admin enrollments.

--- app/models/concerns/enrollment/adminable.rb
+++ app/models/concerns/enrollment/adminable.rb
@@ -3,6 +3,45 @@ module Enrollment::Adminable
 
   included do
     rails_admin do
+
+      list do
+        field :role
+        field :career_attendance              
+        field :career_calendar_management     
+        field :career_customer_service        
+        field :career_networking              
+        field :career_problem_solving         
+        field :career_punctuality             
+        field :career_quality_of_work         
+        field :career_response_to_supervision 
+        field :career_summary  
+        field :career_taking_initiative       
+        field :career_task_management         
+        field :career_teamwork                
+        field :career_total                   
+        field :career_workplace_appearance    
+        field :career_workplace_culture       
+        field :communication_nonverbal        
+        field :communication_summary
+        field :communication_total            
+        field :communication_verbal           
+        field :communication_written          
+        field :emotional_intelligence
+        field :id_from_canvas
+        field :technical_good_questions       
+        field :technical_progress             
+        field :user do
+          queryable true
+          sticky true
+          searchable [:canvas_full, :piazza_full]
+        end
+
+        field :cohort do
+          queryable true
+          searchable [:name]
+        end
+      end
+
       edit do
         include_fields :career_attendance,
           :career_calendar_management,
