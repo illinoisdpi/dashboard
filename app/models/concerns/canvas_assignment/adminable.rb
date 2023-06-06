0e2528c28b93cf441381b01440082de30db2c1e0
Ian Heraty <heratyian@gmail.com>
2023-06-06 10:11:11 -0500

    Add points_possible to CanvasAssignment::Adminable so we can update the points possible on assignments

--- app/models/concerns/canvas_assignment/adminable.rb
+++ app/models/concerns/canvas_assignment/adminable.rb
@@ -4,7 +4,7 @@ module CanvasAssignment::Adminable
   included do
     rails_admin do
       edit do
-        include_fields :authentication, :css, :databases, :domain_modeling, :excluded, :html, :javascript, :rails, :ruby, :weight, :position
+        include_fields :authentication, :css, :databases, :domain_modeling, :excluded, :html, :javascript, :rails, :ruby, :weight, :position, :points_possible
       end
     end
   end
