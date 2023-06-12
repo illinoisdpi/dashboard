db6525a12448e6eb4e8c82f90838e07b543a5b52
Ian Heraty <heratyian@gmail.com>
2023-06-12 12:29:15 -0500

    Add snapshot attributes to adminable

--- app/models/concerns/user/adminable.rb
+++ app/models/concerns/user/adminable.rb
@@ -14,7 +14,13 @@ module User::Adminable
           :personal_website,
           :quote,
           :roles,
-          :strengths
+          :strengths,
+          :fun_fact,
+          :first_name,
+          :middle_name,
+          :last_name,
+          :one_liner,
+          :skills_and_projects
       end
     end
   end
