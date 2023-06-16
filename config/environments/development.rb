6eb2281c0192d5567df8cf36fd9ec1e2d17a1685
Ian Heraty <heratyian@gmail.com>
2023-06-16 11:26:19 -0500

    Add default url options to dev and prod environments.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  routes.default_url_options = {host: "localhost", port: 3000}
 
   Bullet.enable = true
   Bullet.console = true
