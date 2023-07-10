dae1e5c60b51063fbd40a34b479caf6ce5434208
Ian Heraty <heratyian@gmail.com>
2023-07-10 09:08:35 -0500

    Add canvas cumulative/most recent column charts to cohort overview.

--- app/policies/cohort_policy.rb
+++ app/policies/cohort_policy.rb
@@ -41,7 +41,11 @@ class CohortPolicy < ApplicationPolicy
     user.admin? || user.cohort_instructor?(record) || user.cohort_teaching_assistant?(record)
   end
 
-  def canvas_total_points?
+  def canvas_cumulative_points?
+    user.admin? || user.cohort_instructor?(record)
+  end
+
+  def canvas_point_total_most_recent?
     user.admin? || user.cohort_instructor?(record)
   end
 
