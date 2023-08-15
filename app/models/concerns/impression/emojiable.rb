298451c096204e7ff3f8f2f7441590edf8f1ad69
Dantexkilljoy <doyledustin9@gmail.com>
2023-08-15 12:53:56 -0500

    added new line and fixed indentation

--- app/models/concerns/impression/emojiable.rb
+++ app/models/concerns/impression/emojiable.rb
@@ -1,21 +1,21 @@
 module Impression::Emojiable
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
+
+  included do
+    validates :emoji, emoji: true
+  end
   
-    included do
-        validates :emoji, emoji: true
-    end
-    
-    EMOJIS = {
-        👍: "positive",
-        👎: "negative",
-        🙋: "asking questions",
-        😇: "helping others",
-        🥳: "growth",
-        😬: "unprofessional",
-        😠: "lashing out",
-        🤩: "all star",
-        😶: "lack communication",
-        😑: "lack progress",
-        ⏰: "time management"
-    }.freeze
+  EMOJIS = {
+    👍: "positive",
+    👎: "negative",
+    🙋: "asking questions",
+    😇: "helping others",
+    🥳: "growth",
+    😬: "unprofessional",
+    😠: "lashing out",
+    🤩: "all star",
+    😶: "lack communication",
+    😑: "lack progress",
+    ⏰: "time management"
+  }.freeze
 end
