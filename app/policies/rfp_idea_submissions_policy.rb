c88035c1b779dd5d991440b2e9974fb938a69af9
ajmompr <amompremier@gmail.com>
2024-02-25 18:13:47 -0600

    updated logic on rfp_idea_submissions policy to allow users to view and create new submissions

--- app/policies/rfp_idea_submissions_policy.rb
+++ app/policies/rfp_idea_submissions_policy.rb
@@ -1,17 +1,21 @@
 class RfpIdeaSubmissionsPolicy < ApplicationPolicy
   def index?
+    true
   end
 
   def show?
+    true
   end
 
   def new?
+    true
   end
-
-  def edit?
-  end
-
+  
   def create?
+    true
+  end
+  
+  def edit?
   end
 
   def update?
