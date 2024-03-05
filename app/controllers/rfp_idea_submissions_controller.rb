39f30a4dcbb2770b5aef0252cca7eb769e0c4c56
Alexandra Mompremier <amompremier@gmail.com>
2024-03-05 15:01:12 -0600

    removed index, show, edit, update & destroy actions from rfp_idea_submissions controller

--- app/controllers/rfp_idea_submissions_controller.rb
+++ app/controllers/rfp_idea_submissions_controller.rb
@@ -5,22 +5,10 @@ class RfpIdeaSubmissionsController < ApplicationController
   before_action { authorize(:rfp_idea_submissions) }
   before_action :set_rfp_idea_submission, only: %i[ show edit update destroy ]
   
-  # GET /rfp_idea_submissions or /rfp_idea_submissions.json
-  def index
-    @rfp_idea_submissions = RfpIdeaSubmission.all
-  end
-
-  # GET /rfp_idea_submissions/1 or /rfp_idea_submissions/1.json
-  def show
-  end
 
   # GET /rfp_idea_submissions/new
   def new
     @rfp_idea_submission = RfpIdeaSubmission.new
-  end
-
-  # GET /rfp_idea_submissions/1/edit
-  def edit
   end
 
   # POST /rfp_idea_submissions or /rfp_idea_submissions.json
@@ -36,29 +24,6 @@ class RfpIdeaSubmissionsController < ApplicationController
         format.html { redirect_to new_rfp_idea_submission_path }
         format.json { render json: @rfp_idea_submission.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /rfp_idea_submissions/1 or /rfp_idea_submissions/1.json
-  def update
-    respond_to do |format|
-      if @rfp_idea_submission.update(rfp_idea_submission_params)
-        format.html { redirect_to rfp_idea_submission_url(@rfp_idea_submission), notice: "Rfp idea submission was successfully updated." }
-        format.json { render :show, status: :ok, location: @rfp_idea_submission }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @rfp_idea_submission.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /rfp_idea_submissions/1 or /rfp_idea_submissions/1.json
-  def destroy
-    @rfp_idea_submission.destroy
-
-    respond_to do |format|
-      format.html { redirect_to rfp_idea_submissions_url, notice: "Rfp idea submission was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
