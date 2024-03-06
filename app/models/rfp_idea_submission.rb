99185b71a612c3381f7f46c41e20a557b88e1ae9
Alexandra Mompremier <amompremier@gmail.com>
2024-03-06 10:49:34 -0600

    updated validation key name to :contact_email  & re-ordered validations

--- app/models/rfp_idea_submission.rb
+++ app/models/rfp_idea_submission.rb
@@ -2,18 +2,18 @@
 #
 # Table name: rfp_idea_submissions
 #
-#  id           :uuid             not null, primary key
-#  contact_name :string
-#  details      :text
-#  email        :string
-#  phone_number :string
-#  title        :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id            :uuid             not null, primary key
+#  contact_email :string
+#  contact_name  :string
+#  contact_phone :string
+#  details       :text
+#  title         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
 #
 class RfpIdeaSubmission < ApplicationRecord
   validates :contact_name, presence: true
+  validates :contact_email, presence: true
   validates :title, presence: true
-  validates :email, presence: true
   validates :details, presence: true
 end
