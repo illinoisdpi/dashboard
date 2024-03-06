b276397dcef77d2fe6bf15449a04a3f9a13680c8
Alexandra Mompremier <amompremier@gmail.com>
2024-03-06 13:18:03 -0600

    removed spacing

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,14 @@ Rails.application.routes.draw do
   constraints subdomain: "dashboard" do
     authenticate :user, ->(user) { ApplicationPolicy.new(user, nil).admin_panel_accessible? } do
       mount RailsAdmin::Engine, at: "admin", as: "rails_admin"
-    end
-    
+    end 
     authenticate :user, ->(user) { ApplicationPolicy.new(user, nil).sidekiq_panel_accessible? } do
       mount Sidekiq::Web => "/sidekiq"
     end
-    
     authenticate :user, ->(user) { ApplicationPolicy.new(user, nil).blazer_panel_accessible? } do
       mount Blazer::Engine, at: "blazer"
     end
-    
     devise_for :users
-    
     resources :cohorts do
       resources :canvas_gradebook_snapshots
       resources :enrollments, module: :cohort do
@@ -29,18 +25,14 @@ Rails.application.routes.draw do
       resources :piazza_activity_reports
     end
     resources :impressions
-    
     root "dashboard#index", as: "dashboard_root"
   end
-  
   constraints subdomain: "news" do
     root "news#index", as: "news_root"
     get "/rss", to: "news#rss"
   end
-  
   constraints subdomain: "rfp" do
     root "rfp#index", as: "rfp_root"
     resources :rfp_idea_submissions, only: [:new, :create]
   end
-
 end
