4dbfbdb34d9a7729eef13e789794ab74e2097ba2
Fidel Leal <fidelxleal@gmail.com>
2024-03-05 22:38:15 -0600

    add more options list styling

--- app/javascript/controllers/search_subject_controller.js
+++ app/javascript/controllers/search_subject_controller.js
@@ -5,17 +5,12 @@ export default class extends Controller {
   static targets = ["inputField", "optionsList", "hiddenField"]
   static values = { cohort: String }
 
-  connect() {
-    console.log("Hello, Stimulus!")
-  }
-
+  
   search() {
     this.optionsListTarget.style.display = "block";
     const inputFieldValue = this.inputFieldTarget.value
     // Append the subject_search parameter as a query string
-    const url = `/cohorts/${
-      this.cohortValue
-    }/enrollments/search?subject_search=${encodeURIComponent(inputFieldValue)}`
+    const url = `/cohorts/${this.cohortValue}/enrollments/search?subject_search=${encodeURIComponent(inputFieldValue)}`
 
     fetch(url, {
       headers: {
@@ -27,20 +22,23 @@ export default class extends Controller {
   
 
   select(event) {
-    this.inputFieldTarget.blur()
-    this.toggleListVisibility()
-    this.inputFieldTarget.value = event.target.innerText
+    this.inputFieldTarget.value = event.target.dataset.subjectName
     this.hiddenFieldTarget.value = event.target.dataset.subjectId
+    this.toggleListVisibility()
+    this.inputFieldTarget.blur()
   }
 
-  toggleListVisibility() {
-    
+  toggleListVisibility() { 
     this.optionsListIsNotVisible() ? this.optionsListTarget.style.display = "block" : this.optionsListTarget.style.display = "none"
-  
+  }
+
+  hideWhenEmpty() {
+    if (this.inputFieldTarget.value === "" && this.inputFieldTarget !== document.activeElement) {
+      this.toggleListVisibility()
+    }
   }
 
   optionsListIsNotVisible() {
-    console.log(!this.optionsListTarget.display === "none")
     return !this.optionsListTarget.display === "none"
   }
 }
