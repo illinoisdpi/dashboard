6f30b144e1872c8d26768a65e81e116ee0e06381
Alexandra Mompremier <amompremier@gmail.com>
2024-04-22 14:48:17 -0500

    destroyed RfpMailer; updating mailer naming to match RfpIdeaSubmissions model

--- spec/mailers/previews/rfp_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/rfp_mailer
-class RfpMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/rfp_mailer/new_rfp_idea_submitted
-  def new_rfp_idea_submitted
-    RfpMailer.new_rfp_idea_submitted
-  end
-
-end
