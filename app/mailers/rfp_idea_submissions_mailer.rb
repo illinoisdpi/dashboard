957b56536c55d4e8452a371d2c715b326bfce59a
Alexandra Mompremier <amompremier@gmail.com>
2024-04-22 14:55:18 -0500

    corrected method name spelling to 'confirmation'

--- app/mailers/rfp_idea_submissions_mailer.rb
+++ app/mailers/rfp_idea_submissions_mailer.rb
@@ -1,13 +1,12 @@
 class RfpIdeaSubmissionsMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.rfp_idea_submissions_mailer.confimration.subject
   #
-  def confimration
+  def confirmation
     @greeting = "Hi"
 
-    mail to: "to@example.org"
+    mail(to: "amompremier@gmail.com", subject: "RFP Idea Submission Received")
   end
 end
