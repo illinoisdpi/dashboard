9ca9deeccbdc3043055fd8eb0812ba8f1a3305b2
Alexandra Mompremier <amompremier@gmail.com>
2024-04-22 11:37:21 -0500

    generated rfp_idea_submissions mailer

---
+++ app/mailers/rfp_idea_submissions_mailer.rb
@@ -0,0 +1,2 @@
+class RfpIdeaSubmissionsMailer < ApplicationMailer
+end
---
+++ spec/mailers/previews/rfp_idea_submissions_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/rfp_idea_submissions_mailer
+class RfpIdeaSubmissionsMailerPreview < ActionMailer::Preview
+
+end
