a1e23598a9e3b29332abe7897b60f7b1c34a8c82
Alexandra Mompremier <amompremier@gmail.com>
2024-05-28 10:39:53 -0500

    updated impression emoji keys to reflect 13 durable/soft skill categories

--- app/models/concerns/impression/emojiable.rb
+++ app/models/concerns/impression/emojiable.rb
@@ -4,32 +4,35 @@ module Impression::Emojiable
   included do
     validates :emoji, emoji: true
   end
-  
+
   def emoji_name
     Impression::EMOJIS[emoji.to_sym] || Emoji.find_by_unicode(emoji).name
   end
 
   EMOJIS = {
+    🧥: "workplace appearance",
+    🕴️: "workplace culture and policy",
+    🙌: "follow-through",
+    ✅: "quality of work",
+    🏁: "taking initiative",
+    💬: "communication skills",
+    🫡: "response to supervision",
+    🤝: "teamwork",
+    🛜: "networking",
+    🤔: "problem solving",
+    💪: "resilience",
+    🪞: "self-awareness",
+    🤗: "attitude"
     👍: "positive",
     👎: "negative",
     🙋: "asking questions",
     😇: "helping others",
     🥳: "growth",
     😬: "unprofessional",
-    😠: "lashing out",
     🤩: "all star",
-    😶: "lack communication",
-    😑: "lack progress",
     ⏰: "time management",
-    ✅: "consistent",
-    ❌: "inconsistent",
-    🏁: "commited",
-    🚷: "lack of follow-through",
     😎: "confident",
     🙈: "lack of confidence",
-    💬: "good communication",
-    🤐: "lacking communication",
-    🤝: "collaboration",
     🚧: "conflict/lack of collaboration"
   }.freeze
 end
