a5e83610ab6a652cea691a5d97b337124150f1cc
Calvin Armstrong <calvin.r.armstrong@gmail.com>
2024-09-04 14:13:21 -0500

    integer to string

--- db/migrate/20240904181154_add_technical_rating_to_enrollments.rb
+++ db/migrate/20240904181154_add_technical_rating_to_enrollments.rb
@@ -1,5 +1,5 @@
 class AddTechnicalRatingToEnrollments < ActiveRecord::Migration[7.0]
   def change
-    add_column :enrollments, :technical_rating, :integer
+    add_column :enrollments, :technical_rating, :string
   end
 end
