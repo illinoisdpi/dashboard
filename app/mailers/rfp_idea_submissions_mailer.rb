8f58213089f2c3dc45794f5c7dcfeb21ccf0c1d0
Calvin Armstrong <calvin.r.armstrong@gmail.com>
2024-09-04 12:57:34 -0500

    Fixed typos all over the place

--- app/mailers/rfp_idea_submissions_mailer.rb
+++ app/mailers/rfp_idea_submissions_mailer.rb
@@ -2,7 +2,7 @@ class RfpIdeaSubmissionsMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.rfp_idea_submissions_mailer.confimration.subject
+  #   en.rfp_idea_submissions_mailer.confirmation.subject
   #
   def confirmation(rfp_idea_submission)
     @rfp_idea_submission = rfp_idea_submission
--- spec/mailers/rfp_idea_submissions_mailer_spec.rb
+++ spec/mailers/rfp_idea_submissions_mailer_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe RfpIdeaSubmissionsMailer, type: :mailer do
-  describe "confimration" do
-    let(:mail) { RfpIdeaSubmissionsMailer.confimration }
+  describe "confirmation" do
+    let(:mail) { RfpIdeaSubmissionsMailer.confirmation }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Confimration")
+      expect(mail.subject).to eq("Confirmation")
       expect(mail.to).to eq(["to@example.org"])
       expect(mail.from).to eq(["from@example.com"])
     end
