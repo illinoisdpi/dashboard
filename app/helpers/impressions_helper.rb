76efb8319d0df60ceaed9c39f53a43effaa4be5c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-09-13 15:36:32 -0500

    Create a helper to create an array of hashes for rendering

--- app/helpers/impressions_helper.rb
+++ app/helpers/impressions_helper.rb
@@ -1,2 +1,11 @@
 module ImpressionsHelper
+  def formatted_emoji_buttons(emoji_array)
+    emoji_array.map do |emoji|
+      {
+        emoji: emoji,
+        category: Impression::Emojiable::EMOJI_CATEGORIES[emoji],
+        description: Impression::Emojiable::EMOJI_DESCRIPTIONS[emoji]
+      }
+    end
+  end
 end
