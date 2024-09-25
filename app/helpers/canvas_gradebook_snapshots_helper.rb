917580ec5805caf701382baba0c432e62f4e4da3
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-09-25 12:09:39 -0500

    Created redcarpet helper

--- app/helpers/canvas_gradebook_snapshots_helper.rb
+++ app/helpers/canvas_gradebook_snapshots_helper.rb
@@ -1,2 +1,9 @@
 module CanvasGradebookSnapshotsHelper
+  def markdown_renderer
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
+  end
+  def render_markdown(text)
+    markdown_renderer.render(text).html_safe
+  end
 end
