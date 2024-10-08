7d7a95bfcae24c6d1784038de880e602f53658b6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-10-08 16:44:53 -0500

    Add TODOs

--- app/policies/impression_policy.rb
+++ app/policies/impression_policy.rb
@@ -1,6 +1,7 @@
 class ImpressionPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
+      # TODO: Change to limit to cohort_ for TA and staff
       if user.admin? || user.instructor?
         scope.all
       else
@@ -13,6 +14,7 @@ class ImpressionPolicy < ApplicationPolicy
     user.admin? || user.instructor? || user.teaching_assistant? || user.staff?
   end
 
+  # TODO: Change to limit to cohort_
   def show?
     user.admin? || user.instructor? || (record.is_a?(Impression) && user == record.author) || user.teaching_assistant? || user.staff?
   end
@@ -37,6 +39,7 @@ class ImpressionPolicy < ApplicationPolicy
     user.admin? || (record.is_a?(Impression) && user == record.author)
   end
 
+  # TODO: Change to limit to cohort_
   def download_csv?
     user.admin? || user.instructor? || user.teaching_assistant? || user.staff?
   end
