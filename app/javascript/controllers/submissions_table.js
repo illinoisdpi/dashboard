a144e74fcdb9bbb500716a0c0e21266c6df3d219
Calvin Armstrong <calvin.r.armstrong@gmail.com>
2024-10-15 16:29:07 -0500

    Fixed datatables

--- app/javascript/controllers/submissions_table.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@hotwired/stimulus';
-
-export default class extends Controller {
-  connect() {
-    const table = $(this.element).find('#submissionsTable').DataTable({
-      ordering: true, // Enable sorting
-    });
-  }
-}
