d1257f4a6ad90d23570f27c94ad558a0ee42118f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-10-16 15:52:58 -0500

    Fix multiple mde editors issue

--- app/javascript/controllers/easymde_init.js
+++ app/javascript/controllers/easymde_init.js
@@ -1,15 +1,18 @@
 document.addEventListener("turbo:load", function() {
   document.querySelectorAll('.easymde').forEach(function(textarea) {
-    new EasyMDE({ 
-      element: textarea,
-      autoDownloadFontAwesome: false,
-      toolbar: [
-        "bold", "italic", "heading", "|", 
-        "quote", "unordered-list", "ordered-list", "|", 
-        "preview", "side-by-side", "fullscreen", "|", 
-        "guide"
-      ],
-      spellChecker: true,
-    });
+    if (!textarea.classList.contains('easymde-initialized')) {
+      new EasyMDE({ 
+        element: textarea,
+        autoDownloadFontAwesome: false,
+        toolbar: [
+          "bold", "italic", "heading", "|", 
+          "quote", "unordered-list", "ordered-list", "|", 
+          "preview", "side-by-side", "fullscreen", "|", 
+          "guide"
+        ],
+        spellChecker: true
+      });
+      textarea.classList.add('easymde-initialized');
+    }
   });
 });
