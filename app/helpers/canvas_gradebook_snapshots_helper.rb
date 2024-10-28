f53560646c6bb17eacb9b40d68afe96bf48419ac
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-10-28 13:35:59 -0500

    Move render to application helper

--- app/helpers/canvas_gradebook_snapshots_helper.rb
+++ app/helpers/canvas_gradebook_snapshots_helper.rb
@@ -1,9 +1,2 @@
 module CanvasGradebookSnapshotsHelper
-  def markdown_renderer
-    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
-    Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
-  end
-  def render_markdown(text)
-    markdown_renderer.render(text).html_safe
-  end
 end
