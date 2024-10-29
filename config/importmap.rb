561d8bbe4193ccf7460eb95abe6264f2e8dd036a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-10-29 11:27:22 -0500

    Implement trix

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,8 +4,8 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "trix", to: "https://unpkg.com/trix@latest/dist/trix.umd.min.js"
-pin "@rails/actiontext", to: "https://cdn.jsdelivr.net/npm/@rails/actiontext@6.1.4.4/dist/actiontext.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "easymde", to: "https://unpkg.com/easymde/dist/easymde.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
