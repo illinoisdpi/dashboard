8a6de2e8716bd5c0bbe556b5c92dad0bb16edbb1
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-11-04 12:04:22 -0600

    Handle error

--- app/uploaders/user/headshot_uploader.rb
+++ app/uploaders/user/headshot_uploader.rb
@@ -1,3 +1,4 @@
+# app/uploaders/user/headshot_uploader.rb
 class User::HeadshotUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
@@ -5,10 +6,19 @@ class User::HeadshotUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Sets folder in cloudinary
+  # Sets folder in Cloudinary
   def public_id
     store_dir
   end
 
   process tags: ["headshot"]
+
+  # Validate file size before uploading
+  def cache!(new_file)
+    if new_file.size > 10.megabytes
+      raise CarrierWave::ProcessingError, "File size exceeds the 10 MB limit. Please upload a smaller file."
+    else
+      super(new_file)
+    end
+  end
 end
