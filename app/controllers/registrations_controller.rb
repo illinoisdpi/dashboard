a42f59348e9c4d5b867022fb352224c071ec6b07
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-11-05 16:18:56 -0600

    Modify update_resource method`

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    if params[:current_password].present?
+      resource.update_with_password(params)
+    else
+      resource.update_without_password(params)
+    end
   end
 end
