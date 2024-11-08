0fdd36a8e8d0314a04ee08c9212d4568b458c0ee
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-11-08 11:41:38 -0600

    Make csv downloadable

--- app/controllers/canvas_gradebook_snapshots_controller.rb
+++ app/controllers/canvas_gradebook_snapshots_controller.rb
@@ -14,6 +14,42 @@ class CanvasGradebookSnapshotsController < ApplicationController
     @canvas_gradebook_snapshots = policy_scope(@cohort.canvas_gradebook_snapshots.default_order)
   end
 
+  def show
+    respond_to do |format|
+      format.html
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"canvas_gradebook_snapshot.csv\""
+        headers['Content-Type'] ||= 'text/csv'
+  
+        csv_data = CSV.generate do |csv|
+          canvas_assignments = @canvas_gradebook_snapshot.canvas_assignments.default_order
+  
+          headers = ['User', 'Role'] + canvas_assignments.unscope(:order).pluck(:name)
+          csv << headers
+  
+          points_possible_row = ['Points Possible', ''] + canvas_assignments.unscope(:order).pluck(:points_possible)
+          csv << points_possible_row
+  
+          @canvas_gradebook_snapshot.enrollments.each do |enrollment|
+            canvas_submissions = @canvas_gradebook_snapshot.canvas_submissions.where(enrollment: enrollment).index_by(&:canvas_assignment_id)
+            row = [
+              enrollment.user.to_s,
+              enrollment.role.to_s,
+            ]
+            canvas_assignments.each do |canvas_assignment|
+              points = canvas_submissions[canvas_assignment.id]&.points
+              row << points
+            end
+            csv << row
+          end
+        end
+  
+        render plain: csv_data
+      end
+    end
+  end
+  
+
   # GET /canvas_gradebook_snapshots/new
   def new
     @breadcrumbs = [
