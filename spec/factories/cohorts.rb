9910b7b787c9291c9407eac175cf3bd6aa6cf8dc
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-11-12 09:57:55 -0600

    add annotations to factories

--- spec/factories/cohorts.rb
+++ spec/factories/cohorts.rb
@@ -1,9 +1,24 @@
+# == Schema Information
+#
+# Table name: cohorts
+#
+#  id                   :uuid             not null, primary key
+#  canvas_shortname     :string
+#  month                :integer          not null
+#  name                 :string
+#  number               :integer          not null
+#  piazza_course_number :string
+#  started_on           :date
+#  year                 :integer          not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
 FactoryBot.define do
   factory :cohort do
-    canvas_shortname { "Test-WE-2020-9.1-INTRO" }
-    year { 2020 }
-    generation { 9 }
-    number { 1 }
+    canvas_shortname { "WE-2022-1.2-SDF" }
+    year { 2022 }
+    month { 1 }
+    number { 2 }
     started_on { Date.today }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,42 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :uuid             not null, primary key
+#  canvas_full            :string
+#  career_highlights      :text
+#  devto_username         :string
+#  discord_username       :string
+#  education              :text
+#  email                  :citext           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  fun_fact               :text
+#  github_username        :string
+#  headshot               :string
+#  languages              :text
+#  last_name              :string
+#  middle_name            :string
+#  most_recent_role       :text
+#  one_liner              :text
+#  personal_website       :string
+#  piazza_full            :string
+#  quote                  :text
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  skills_and_projects    :text
+#  strengths              :text
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  discord_id             :string
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_github_username       (github_username) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 FactoryBot.define do
     factory :user do
       email { Faker::Internet.unique.email }
@@ -6,4 +45,3 @@ FactoryBot.define do
       last_name { "Doe" }
     end
   end
-  
