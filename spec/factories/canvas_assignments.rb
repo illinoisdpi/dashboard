6158e3e1d3f22913488a43ed7f35547655ae9012
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2025-01-29 15:26:14 -0600

    Annotate fatories

--- spec/factories/canvas_assignments.rb
+++ spec/factories/canvas_assignments.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: canvas_assignments
+#
+#  id              :uuid             not null, primary key
+#  authentication  :integer
+#  css             :integer
+#  databases       :integer
+#  domain_modeling :integer
+#  excluded        :boolean          default(FALSE)
+#  html            :integer
+#  id_from_canvas  :string
+#  javascript      :integer
+#  name            :string
+#  points_possible :integer
+#  position        :integer
+#  rails           :integer
+#  ruby            :integer
+#  weight          :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  cohort_id       :uuid             not null
+#
+# Indexes
+#
+#  index_canvas_assignments_on_cohort_id  (cohort_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (cohort_id => cohorts.id)
+#
 FactoryBot.define do
     factory :canvas_assignment do
       cohort
--- spec/models/canvas_gradebook_snapshot_spec.rb
+++ spec/models/canvas_gradebook_snapshot_spec.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: canvas_gradebook_snapshots
+#
+#  id            :uuid             not null, primary key
+#  csv_filename  :string
+#  downloaded_at :datetime
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  cohort_id     :uuid             not null
+#  user_id       :uuid             not null
+#
+# Indexes
+#
+#  index_canvas_gradebook_snapshots_on_cohort_id  (cohort_id)
+#  index_canvas_gradebook_snapshots_on_user_id    (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (cohort_id => cohorts.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe CanvasGradebookSnapshot, type: :model do
