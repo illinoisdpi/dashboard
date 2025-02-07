4a9692d7014c09cd203a58dc2b6e068763f96cf8
Ian Heraty <heratyian@gmail.com>
2025-02-07 14:24:25 -0600

    Add bootstrap form-check to impression staff_only and remove unneeded stimulus controller import (since it's already lazy loaded)

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,7 @@
 import { Application } from "@hotwired/stimulus"
-import TooltipController from "./tooltip_controller"
 
 const application = Application.start()
-application.register("tooltip", TooltipController)
 
 application.debug = false
-window.Stimulus = application
 
 export { application }
