fc879cd98a4577aeb040067bfd7717a2ce2800b6
aldo portillo <94483081+aldoportillo@users.noreply.github.com>
2025-02-07 10:55:30 -0600

    set default false to migrations and delete one_off

--- db/migrate/20250131173635_add_staff_only_to_impressions.rb
+++ db/migrate/20250131173635_add_staff_only_to_impressions.rb
@@ -1,5 +1,5 @@
 class AddStaffOnlyToImpressions < ActiveRecord::Migration[7.0]
   def change
-    add_column :impressions, :staff_only, :boolean
+    add_column :impressions, :staff_only, :boolean, default: false
   end
 end
