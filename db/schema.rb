c980d7c328a4d8c9144879ffc11337e17b864821
aldo portillo <94483081+aldoportillo@users.noreply.github.com>
2025-03-11 12:07:23 -0500

    Migration changes fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2025_01_24_174951) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
