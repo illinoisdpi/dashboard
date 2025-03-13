dfcf0b6207b3638f1320685e81b710a954962b6c
aldo portillo <94483081+aldoportillo@users.noreply.github.com>
2025-03-13 13:40:16 -0500

    Remove reset column

--- db/migrate/20250313182249_add_attendees_count_to_attendance.rb
+++ db/migrate/20250313182249_add_attendees_count_to_attendance.rb
@@ -2,7 +2,6 @@ class AddAttendeesCountToAttendance < ActiveRecord::Migration[7.0]
   def up
     add_column :attendances, :attendees_count, :integer, default: 0, null: false
 
-    Attendance.reset_column_information
     Attendance.find_each do |attendance|
       Attendance.reset_counters(attendance.id, :attendees)
     end
